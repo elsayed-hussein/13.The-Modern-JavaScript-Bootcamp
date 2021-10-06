2eb77a9264e49f14d11a0a48a403f4a502ab9939
elsayed-hussen <elsayed.hussen@gmail.com>
2021-10-06 17:38:27 +0200

    03-7-don

---
+++ basics-Answer/03-05-Answer.js
@@ -0,0 +1,6 @@
+let fah = 60;
+let cel = ((fah - 32) * 5) / 9;
+let kel = ((fah + 459.67) * 5) / 9;
+
+console.log(cel);
+console.log(kel);
---
+++ basics-Answer/03-06-Answer.js
@@ -0,0 +1,9 @@
+let age = 65;
+
+let isChild = age <= 7;
+
+let isSenior = age >= 65;
+
+console.log(isChild);
+
+console.log(isSenior);
