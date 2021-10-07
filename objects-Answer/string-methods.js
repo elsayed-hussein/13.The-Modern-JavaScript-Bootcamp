0b5f5836124be806c02d8dc5010a2d2293bf2430
elsayed hussen <elsayed.hussen@gmail.com>
2021-10-07 14:57:58 +0200

    05-don

---
+++ objects-Answer/string-methods.js
@@ -0,0 +1,12 @@
+let name = "  Andrew Mead ";
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+let password = "abc123asdf098";
+console.log(password.includes("password"));
+
+console.log(name.trim());
